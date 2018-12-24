420aa45038623f02872b058c7b60b1d66084c9a2
liugq <liugq5713@gmail.com>
2018-12-24 18:17:57 +0800

    use hashRouter

--- src/views/layout/index.js
+++ src/views/layout/index.js
@@ -4,12 +4,12 @@ import Content from '../content'
 import Header from '../header'
 import Congratulation from '../congratulation'
 import './index.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter  as Router, Route} from 'react-router-dom'
 
 export default class Layout extends Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename={process.env.PUBLIC_URL} >
         <div>
           <div className='has-background-white'>
             <Header />
