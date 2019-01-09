174fde3d446b2a85ae38688adc118dd3b6cfe1c1
Travis CI User <travis@example.org>
2019-01-09 10:41:34 +0000

    deploy

---
+++ precache-manifest.11b1c52ea5fb7ff88107b3180d64a71d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "424b11c981f32b1e3213",
+    "url": "/pomodoro/static/css/main.6c8a0018.chunk.css"
+  },
+  {
+    "revision": "424b11c981f32b1e3213",
+    "url": "/pomodoro/static/js/main.424b11c9.chunk.js"
+  },
+  {
+    "revision": "fb4ec3b06389baf53023",
+    "url": "/pomodoro/static/css/1.7c896521.chunk.css"
+  },
+  {
+    "revision": "fb4ec3b06389baf53023",
+    "url": "/pomodoro/static/js/1.fb4ec3b0.chunk.js"
+  },
+  {
+    "revision": "a702e67ff471c6ecfc33",
+    "url": "/pomodoro/static/js/runtime~main.a702e67f.js"
+  },
+  {
+    "revision": "6bca466f24191df512dda6a32e19963a",
+    "url": "/pomodoro/static/media/logo.6bca466f.svg"
+  },
+  {
+    "revision": "28576b527435444a02cd4daafdd6e227",
+    "url": "/pomodoro/static/media/congratulation.28576b52.gif"
+  },
+  {
+    "revision": "4841e0efe35994c5e7b5051476b89722",
+    "url": "/pomodoro/index.html"
+  }
+];
